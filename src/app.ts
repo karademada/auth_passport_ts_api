702c8e89826eefe60e6ef5a0e42925f2a488b450
christophe_lazantsy <christophe_lazantsy@sawext.carrefour.com>
2019-06-22 11:24:59 +0200

    add jest and unit test to the server api

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,4 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.listen(5000, () => console.log('Example app listening on port 5000'))
+app.listen(5000, () => console.log('Example app listening on port 5000')) 
